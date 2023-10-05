1c11e483a33585bacc1163dcf9350da0137bf014
aryash19 <Aryashchaudhary12341234@gmail.com>
2023-10-05 20:15:00 +0530

    new

--- src/pages/Hero.js
+++ src/pages/Hero.js
@@ -12,13 +12,15 @@ import c2 from '../Assets/image2.jpg'
 import c3 from '../Assets/image3.jpg'
 import c4 from '../Assets/image4.jpg'
 import c5 from '../Assets/image5.jpg'
+import c6 from '../Assets/image6.jpg'
+
 
 const Hero = () => {
   return (
     <>
 
       <div className="">
-      <Carousel showStatus={false} showThumbs={false} showIndicators={false} infiniteLoop={true} autoPlay="true">
+      <Carousel showStatus={false} showThumbs={false} showIndicators={false} infiniteLoop={true} >
   <div style={{ position: 'relative' }}>
     <img src={c1} alt='err' />
     <Button
@@ -129,6 +131,28 @@ const Hero = () => {
       <Link to='/contact' style={{ textDecoration: 'none', color: '#fff' }}>Get a Call from Us</Link>
     </Button>
   </div>
+  <div style={{ position: 'relative' }}>
+    <img src={c6} alt='err' />
+    <Button
+      sx={{
+        position: 'absolute',
+        top: '90%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        backgroundColor: '#FF971E',
+        color: '#fff',
+        borderRadius: '10px',
+        zIndex: 99,
+        '&:hover': {
+          backgroundColor: '#FFB55F',
+          color: '#fff',
+        },
+      }}
+      variant="contained"
+    >
+      <Link to='/contact' style={{ textDecoration: 'none', color: '#fff' }}>Get a Call from Us</Link>
+    </Button>
+    </div>
 </Carousel>
 
         
